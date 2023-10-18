9266c11a2cb17db363be12b5778271b60cf63171
Derek Novak <derek.novak1@gmail.com>
2023-10-18 18:31:12 -0500

    quick fix

--- ruby_small_problems/easy3/exercise10.rb
+++ ruby_small_problems/easy3/exercise10.rb
@@ -2,9 +2,16 @@ def uppercase?(str)
   str == str.upcase
 end
 
-puts uppercase?('t') == false
-puts uppercase?('T') == true
-puts uppercase?('Four Score') == false
-puts uppercase?('FOUR SCORE') == true
-puts uppercase?('4SCORE!') == true
-puts uppercase?('') == true
+=begin
+
+From lines 1 - 3, the uppercase? method is defined with 1 parameter: str.
+Within the method, the conditional operator == is employed, which checks
+whether the returned value of the upcase method's call on str is equal to str
+itself. If evaluated as true, the method will return the boolean object true;
+otherwise, it will return false.
+
+The method uppercase?, when passed an empty string object, ought to return
+false. Returning true from the method is deceptive as an empty string has no
+character-case and therefore cannot be considered 'uppercase'.
+
+=end
