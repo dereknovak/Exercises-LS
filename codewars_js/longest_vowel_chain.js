f8f69d494d0a0b43452131f590d9398a5ba75f50
Derek Novak <derek.novak1@gmail.com>
2024-09-11 12:09:48 -0500

    Completed several CodeWars problems

--- codewars_js/longest_vowel_chain.js
+++ codewars_js/longest_vowel_chain.js
@@ -1,5 +1,5 @@
 function longestVowelChain(string) {
-  return Math.max(...string.match(/[aeiou]+/ig).map(chain => chain.length));
+  return Math.max(...(string.match(/[aeiou]+/ig) || ['']).map(chain => chain.length));
 }
 
 console.log(longestVowelChain("codewarriors") === 2);
@@ -10,3 +10,4 @@ console.log(longestVowelChain("strengthlessnesses") === 1);
 console.log(longestVowelChain("cuboideonavicuare") === 2);
 console.log(longestVowelChain("chrononhotonthuooaos") === 5);
 console.log(longestVowelChain("iIihoovaeaAaOougjyaw") === 8 );
+console.log(longestVowelChain("brdthyp") === 0 );
